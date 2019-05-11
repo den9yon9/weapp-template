a38b3f45af6727205d3568983cab11d1b7c560cb
yong <den9yon9@gmail.com>
2019-05-11 17:34:43 +0800

    fix: 优化wx.chooseImageSync函数

--- utils/init.js
+++ utils/init.js
@@ -6,35 +6,39 @@ export default function init(wx) {
   }
 
   wx.showModalSync = function (content, {
-    title = '提示',
-    showCancel = true
-  } = {}) {
-    return new Promise((resolve, reject) => {
-      wx.showModal({
-        title,
-        content: content.toString(),
-        showCancel,
-        success(res) {
-          if (res.confirm) {
-            resolve(true)
-          } else {
-            resolve(false)
+      title = '提示',
+      showCancel = true
+    } = {}) {
+      return new Promise((resolve, reject) => {
+        wx.showModal({
+          title,
+          content: content.toString(),
+          showCancel,
+          success(res) {
+            if (res.confirm) {
+              resolve(true)
+            } else {
+              resolve(false)
+            }
+          },
+          fail(err) {
+            console.log(err)
+            reject(err)
           }
-        },
-        fail(err) {
-          console.log(err)
-          reject(err)
-        }
+        })
       })
-    })
-  },
+    },
 
-    wx.chooseImageSync = function () {
+    wx.chooseImageSync = function ({
+      count = 1,
+      sizeType = ['original', 'compressed'],
+      sourceType = ['album', 'camera']
+    }) {
       return new Promise((resolve, reject) => {
         wx.chooseImage({
-          count: 1,
-          sizeType: ['original', 'compressed'],
-          sourceType: ['album', 'camera'],
+          count,
+          sizeType,
+          sourceType,
           success(res) {
             // tempFilePath可以作为img标签的src属性显示图片
             resolve(res.tempFilePaths[0])
